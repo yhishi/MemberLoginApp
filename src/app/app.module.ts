bb1b248c27bc00178f04ae3bae24bb9560dd1fbe
yhishi <hishikawakeiki@hishi-Mac.local>
2018-08-22 22:54:44 +0900

    Facebookログイン機能追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Facebook } from '@ionic-native/facebook';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Facebook
   ]
 })
 export class AppModule {}
--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import * as firebase from 'Firebase';
 import { Login } from '../login/login';
 import { Signup } from '../signup/signup';
+import { Facebook } from '@ionic-native/facebook';
+import { TabsPage } from '../tabs/tabs';
+
 /**
  * Generated class for the Welcome page.
  *
@@ -16,19 +19,34 @@ import { Signup } from '../signup/signup';
 })
 export class Welcome {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private facebook:Facebook) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Welcome');
   }
 
-  login(){
+  loginEmail(){
    this.navCtrl.push(Login);
   }
 
-  signup(){
+  signUp(){
    this.navCtrl.push(Signup, {}, {animate:false});
+  }
+
+  loginFacebook(): Promise<any> {
+    return this.facebook.login(['email'])
+      .then( response => {
+        const facebookCredential = firebase.auth.FacebookAuthProvider
+          .credential(response.authResponse.accessToken);
+
+        firebase.auth().signInWithCredential(facebookCredential)
+          .then( success => { 
+            alert("Firebase success: " + JSON.stringify(success)); 
+            this.navCtrl.push(TabsPage);
+          });
+
+      }).catch((error) => { console.log(error) });
   }
 
 }
